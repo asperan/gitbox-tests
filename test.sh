5ebe5a166d40ef9f4682e9e075c3a0f1d7e16739
Alex Speranza <alex.speranza@studio.unibo.it>
2023-11-08 11:02:19 +0100

    test: add test

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "Main branch, no test defined here" >&2
-exit 1
+set -euo pipefail
+set -x
+
+EXPECTED_VERSION="0.2.0"
+
+CALCULATED_VERSION="$(gb describe)"
+
+test "${CALCULATED_VERSION}" == "${EXPECTED_VERSION}"
 
