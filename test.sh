e34d9fd03dbe209949879d54674f5b76a38dbd60
Alex Speranza <alex.speranza@studio.unibo.it>
2023-11-08 10:36:44 +0100

    test: implement test for a scoped commit

--- test.sh
+++ test.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-echo "Main branch, no test defined here" >&2
-exit 1
+set -euo pipefail
+set -x
+
+trap "git reset --hard HEAD~1 >/dev/null" EXIT
+
+EXPECTED_COMMIT_MESSAGE="feat(scope): add a new text file"
+
+touch new_file.txt
+
+git add new_file.txt
+
+gb commit -t 'feat' -S 'scope' -s 'add a new text file' -m '' --no-breaking -q
+
+LAST_COMMIT_MESSAGE="$(git log --format=%s -n 1)"
+
+test "${LAST_COMMIT_MESSAGE}" == "${EXPECTED_COMMIT_MESSAGE}"
 
