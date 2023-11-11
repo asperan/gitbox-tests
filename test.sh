490ac72695aecf3ef4ede0c845f9a1191e5724bd
Alex Speranza <alex.speranza@studio.unibo.it>
2023-11-11 09:43:02 +0100

    test: add test when asking for the version when on a prerelease tag

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "Main branch, no test defined here" >&2
-exit 1
+set -euo pipefail
+set -x
+
+EXPECTED_VERSION="$(if [ "$(git describe | tr -cd '-' | wc -c)" -eq "1" ] ; then git describe | cut -d '-' -f 1 ; else echo "INVALID_VERSION" ; fi )"
+
+CALCULATED_VERSION="$(gb describe)"
+
+test "${CALCULATED_VERSION}" == "${EXPECTED_VERSION}"
 
